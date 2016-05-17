bb62482a9ddcaea3ee1263bf5a312a26fc9c732a
Jared Stander <jaredstander@gmail.com>
2016-05-17 16:56:34 -0400

    Adding the controller, model, and view for votes. Votes view is currently blank.

---
+++ app/models/vote.rb
@@ -0,0 +1,20 @@
+class Vote < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :voteable, polymorphic: true
+
+  attr_accessible :up_voted, :voteable_type
+
+  validates :voteable_id, :user, presence: true
+  validates :user_id, uniqueness: { scope: :voteable_id }
+
+  after_save :update_vote_count
+
+  def update_vote_count
+    if up_voted 
+      self.voteable.vote_count += 1
+    else 
+      self.voteable.vote_count -=1
+    end
+  self.voteable.save
+  end
+end
