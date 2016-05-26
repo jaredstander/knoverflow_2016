57c584331e91130125715d867c9593a4b3036616
Jared Stander <jaredstander@gmail.com>
2016-05-26 16:34:58 -0400

    Votes are ajax responsive, uses simple incrememnt or decrement instead of pulling new number every time to simplify the number of db calls.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,35 +1,28 @@
 class VotesController < ApplicationController
 
   def create
-    @voteable = find_commentable
+    @voteable = find_voteable
     @vote = @voteable.votes.build
     @vote.user = current_user
-    # respond_to do |format|
-      if @vote.save
-        @vote_count = @voteable.votes.count
-        # format.html { redirect_to @vote_count, notice: 'Comment Posted.' }
-        # format.js
-        # format.json { render json: @vote_count, status: :ok }
-        render json: @vote_count, status: :ok
-      else
-        # format.json { render json: @vote_count.errors, status: :unprocessable_entity }
-        render json: @vote.errors, status: :unprocessable_entity
-      end
-    # end
+    if @vote.save
+      # @vote_count = @voteable.votes.count
+      # render json: @vote, status: :ok
+    else
+      render json: @vote.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @vote = Vote.find(params["id"])
+    @voteable = @vote.voteable
     redirect_to root_path unless @vote.user_id == current_user.id
-    @voteable = find_commentable
-    @vote_count = @voteable.votes.count
     @vote.destroy
-    render json: @vote_count, status: :ok
+    # render json: @voteable, status: :ok
   end
 
   private
 
-  def find_commentable
+  def find_voteable
     params.each do |name, value|
       if name =~ /(.+)_id$/
         return $1.classify.constantize.find(value)
